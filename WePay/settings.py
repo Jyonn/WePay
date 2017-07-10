c944b480989599baa2127fb9fbed4f127bafaaf1
adelliu <lqj679ssn@qq.com>
2017-07-10 12:35:35 +0800

    *debug=True & ^\w+

--- WePay/settings.py
+++ WePay/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't-=hpscl(3((_=eydw-anl%o@k7lzf^#lmdiv8)dpy^nn*#iv4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
