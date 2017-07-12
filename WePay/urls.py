8877e6c734e260d9d0d11c097ab7f22d132fb681
adelliu <lqj679ssn@qq.com>
2017-07-12 10:22:00 +0800

    *route.py??

--- WePay/urls.py
+++ WePay/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     url(r'^order$', router.order),
     # url(r'^order\?status=(?P<status>\w+)&page=(?P<page>\d+)&count=(?P<count>\d+)$', router.order_status_page_count),
     url(r'^order/(?P<order_id>\d+)/status$', router.order_order_id_status),
-    # url(r'^\w+$', index_view),
+    url(r'^\w+$', index_view),
     # url(r'^\w+/\w+$', index_view),
 ]
 
